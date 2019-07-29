a4ed08c744ba640805c601396f447a4f809b1ace
= <=>
2019-07-29 15:58:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+// import ScrollToTop from "./utils/ScrollToTopController";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./utils/aos";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="text-white bg-primary text-center display-2 mt-6">
+          Ready To Use React App !!
+          <FontAwesomeIcon icon="smile" />
+        </h1>
+      </div>
     </div>
   );
 }
